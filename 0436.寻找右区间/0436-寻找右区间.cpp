ca577315b278a7d28de36ab5615986d147f962e3
Pang <katepangliu@gmail.com>
2022-05-20 19:36:49 +0800

    Update 0436-寻找右区间.cpp

--- 0436.寻找右区间/0436-寻找右区间.cpp
+++ 0436.寻找右区间/0436-寻找右区间.cpp
@@ -3,19 +3,20 @@ public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
         int n = intervals.size();
         vector<int> ans(n, -1);
-        
+
         vector<pair<int,int>> starts(n);
         for( int i=0; i<n; i++ ) {
-            starts[i] = pair<int,int>(intervals[i][0], i);
+            starts[i] = make_pair(intervals[i][0], i);
         }
         sort(starts.begin(), starts.end());
 
         for( int i=0; i<n; i++ ) {
-            auto low = lower_bound( starts.begin(), starts.end(), pair<int,int>(intervals[i][1], 0) );
+            auto low = lower_bound( starts.begin(), starts.end(), make_pair(intervals[i][1], 0));
             if( low != starts.end() ) {
                 ans[i] = low->second;
             }
         }
+
         return ans;
     }
 };
